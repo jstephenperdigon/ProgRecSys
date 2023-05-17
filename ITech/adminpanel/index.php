d61b529b6b4d0f2af47be4c77b7c94762f00c082
JOHNSTEPHEN_PERDIGON <PERDIGON.JOHNSTEPHEN.BSIT2020@GMAIL.COM>
2023-05-17 20:55:32 +0800

    Added Controller For Admin

--- ITech/adminpanel/index.php
+++ ITech/adminpanel/index.php
@@ -1,6 +1,6 @@
 <?php require_once "../components/controllerUserData.php"; ?>
 <?php require "./components/header.php"; ?>
-<?php require "./query/addExamination.php"; ?>
+<?php require "./query/examinationController.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
 <title>
