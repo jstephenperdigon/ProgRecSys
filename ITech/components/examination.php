07e4fd61b23fa6370e5fed917b215c36290dac63
JOHNSTEPHEN_PERDIGON <PERDIGON.JOHNSTEPHEN.BSIT2020@GMAIL.COM>
2023-05-18 01:44:27 +0800

    Query Added

--- ITech/components/examination.php
+++ ITech/components/examination.php
@@ -26,6 +26,8 @@ if ($email != false && $password != false) {
 $sql = "SELECT * FROM examinationtbl";
 $questions = $con->query($sql);
 
+$sequel = "SELECT * FROM examinationtbl";
+$answers = $con->query($sequel);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -154,10 +156,10 @@ $questions = $con->query($sql);
 
     /* Form */
     .form {
-      width: clamp(320px, 30%, 430px);
+      width: clamp(1000px, 30%, 430px);
       margin: 0 auto;
       border: 1px solid #ccc;
-      border-radius: 0.35rem;
+      border-radius: 50px;
       padding: 1.5rem;
     }
 
@@ -178,12 +180,14 @@ $questions = $con->query($sql);
 
     /* Button */
     .btns-group {
+      margin-top: 20px;
       display: grid;
       grid-template-columns: repeat(2, 1fr);
       gap: 1.5rem;
     }
 
-    .btn {
+    .btn-body{
+      margin-top: 20px;
       padding: 0.75rem;
       display: block;
       text-decoration: none;
@@ -277,9 +281,9 @@ $questions = $con->query($sql);
   </nav>
 
 
-  <div class="container-body">
-    <div class="row">
-      <div class="col-md-8 mx-auto">
+  <div class="container" style="margin-top: 200px;">
+    <div class="row align-items-center">
+      <div class="col">
         <h2 class="text-center mb-4">Time Remaining</h2>
         <div class="text-center">
           <h5 id="timer">01:00:00</h5>
@@ -288,7 +292,7 @@ $questions = $con->query($sql);
     </div>
   </div>
 
-  <form action="#" class="form">
+  <form action="#" class="form container" method="POST">
     <h4 class="text-center">PART 1</h4>
     <!-- Progress bar -->
     <div class="progressbar">
@@ -317,24 +321,29 @@ $questions = $con->query($sql);
       </h4>
       <ul class="answer text-center">
         <li>
-          <input type="radio" id="choiceA" name="choiceA" />
-          <label for="choiceA">ANSWER 1</label>
+          <input type="radio" id="choiceA" name="q1" />
+          <label for="choiceA">
+            <?php
+        $row = mysqli_fetch_assoc($answers)
+          ?>
+        <?php echo $row["option1"] ?>
+        </label>
         </li>
         <li>
-          <input type="radio" id="choiceB" name="choiceB" />
+          <input type="radio" id="choiceB" name="q1" />
           <label for="choiceB">ANSWER 2</label>
         </li>
         <li>
-          <input type="radio" id="choiceC" name="choiceC" />
+          <input type="radio" id="choiceC" name="q1" />
           <label for="choiceC">ANSWER 3</label>
         </li>
         <li>
-          <input type="radio" id="choiceD" name="choiceD" />
+          <input type="radio" id="choiceD" name="q1" />
           <label for="choiceD">ANSWER 4</label>
         </li>
       </ul>
       <div class="">
-        <a href="#" class="btn btn-next width-50 ml-auto">Next</a>
+        <a href="#" class="btn-body btn-next width-50 ml-auto">Next</a>
       </div>
     </div>
     <div class="form-step">
@@ -346,25 +355,25 @@ $questions = $con->query($sql);
       </h4>
       <ul class="answer text-center">
         <li>
-          <input type="radio" id="choiceA1" name="choiceA1" />
+          <input type="radio" id="choiceA1" name="q2" />
           <label for="choiceA1">ANSWER 1</label>
         </li>
         <li>
-          <input type="radio" id="choiceB1" name="choiceB1" />
+          <input type="radio" id="choiceB1" name="q2" />
           <label for="choiceB1">ANSWER 2</label>
         </li>
         <li>
-          <input type="radio" id="choiceC1" name="choiceC1" />
+          <input type="radio" id="choiceC1" name="q2" />
           <label for="choiceC1">ANSWER 3</label>
         </li>
         <li>
-          <input type="radio" id="choiceD1" name="choiceD1" />
+          <input type="radio" id="choiceD1" name="q2" />
           <label for="choiceD1">ANSWER 4</label>
         </li>
       </ul>
       <div class="btns-group">
-        <a href="#" class="btn btn-prev">Previous</a>
-        <a href="#" class="btn btn-next">Next</a>
+        <a href="#" class="btn-body btn-prev">Previous</a>
+        <a href="#" class="btn-body btn-next">Next</a>
       </div>
     </div>
     <div class="form-step">
@@ -376,25 +385,25 @@ $questions = $con->query($sql);
       </h4>
       <ul class="answer text-center">
         <li>
-          <input type="radio" id="choiceA2" name="choiceA2" />
+          <input type="radio" id="choiceA2" name="q3" />
           <label for="choiceA2">ANSWER 1</label>
         </li>
         <li>
-          <input type="radio" id="choiceB2" name="choiceB2" />
+          <input type="radio" id="choiceB2" name="q3" />
           <label for="choiceB2">ANSWER 2</label>
         </li>
         <li>
-          <input type="radio" id="choiceC2" name="choiceC2" />
+          <input type="radio" id="choiceC2" name="q3" />
           <label for="choiceC2">ANSWER 3</label>
         </li>
         <li>
-          <input type="radio" id="choiceD2" name="choiceD2" />
+          <input type="radio" id="choiceD2" name="q3" />
           <label for="choiceD2">ANSWER 4</label>
         </li>
       </ul>
       <div class="btns-group">
-        <a href="#" class="btn btn-prev">Previous</a>
-        <input type="submit" value="Submit" class="btn" />
+        <a href="#" class="btn-body btn-prev">Previous</a>
+        <input type="submit" value="Submit" class="btn-body" />
       </div>
     </div>
   </form>
