c0ba4a6813d8ba3be010b0800d140b4c024f5d11
Ervin <ervinjeffersonmariano@gmail.com>
2023-05-27 03:51:29 +0800

    userid and useremail to answertbl DONE!

--- ITech/components/query/ControllerExamination.php
+++ ITech/components/query/ControllerExamination.php
@@ -1,6 +1,6 @@
-
-
 <?php
+require_once "../controllerUserData.php";
+session_start();
 
 // Retrieve the JSON data from the request
 $data = json_decode(file_get_contents('php://input'), true);
@@ -16,22 +16,35 @@ if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
 
+$currentEmail = $_SESSION['email'];
+$usertable = "SELECT id, email FROM usertable WHERE email = ?";
+$stmt = $con->prepare($usertable);
+$stmt->bind_param("s", $currentEmail);
+$stmt->execute();
+$result = $stmt->get_result();
+$fetch = $result->fetch_assoc();
+$stmt->close();
+
 // Insert the answers into the database
 $insertedRows = 0;
 foreach ($answers as $answer) {
-   
+    $userId = $fetch['id'];
+    $userEmail = $fetch['email'];
     $questionId = $answer['questionId'];
     $ChoiceID = $answer['ChoiceID'];
     $optionName = $answer['optionName'];
-    // Prepare the SQL statement
-   
-    $sql = "INSERT INTO answertbl (questionid, choice, optionname) VALUES ('$questionId', '$ChoiceID', '$optionName')";
 
-    if ($con->query($sql) === TRUE) {
+    // Prepare the SQL statement
+    $sql = "INSERT INTO answertbl (questionid, choice, optionname, userid, useremail) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $con->prepare($sql);
+    $stmt->bind_param("sssis", $questionId, $ChoiceID, $optionName, $userId, $userEmail);
+
+    if ($stmt->execute()) {
         $insertedRows++;
     } else {
         // Error handling if the insertion fails
     }
+    $stmt->close();
 }
 
 // Close the database connection
